202cdb4f4847ef2fa0e42156999cd817f473cb60
Nipun Chandima <86118362+SANCS99@users.noreply.github.com>
2022-03-20 16:26:53 +0530

    hOME PAGE

---
+++ client/src/screens/Homescreen.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Pizza from '../components/Pizza'
+import pizzas from '../pizzadata'
+export default function Homescreen(){
+    return(
+        <div>
+            <div className="row">
+                {pizzas.map(pizza=>{
+                    return <div className="col-md-4">
+                        <div>
+                            <Pizza pizza={pizza}/>
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
