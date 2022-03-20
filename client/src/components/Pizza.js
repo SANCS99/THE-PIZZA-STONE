dd6f420b56473351614b53c1e123cc5943ac4cf7
Nipun Chandima <86118362+SANCS99@users.noreply.github.com>
2022-03-20 18:20:25 +0530

    Home page
    Calculation and adding some buttons

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React ,{useState} from "react";
 
 export default function Pizza({pizza}) {
+const[quantity, setquantity]=useState(1)
+const[varient, setvarient]=useState('small')
+
     return(
-        <div>
-            <h1>{pizza.name}</h1>
+        <div style={{margin:"100px"}}className="shadow-lg p-3 mb-5 bg-white rounded">
+            <h4>{pizza.name}</h4>
             <img src={pizza.image} className="img-fluid" style={{height:'200px' , width:'200px'}}/>
 
-                    <div className="flex-container">
-                        <div className="w-100">
+                    <div className="flix-container">
+                        <div className="w-100 m-1">
                             <p>Varients</p>
-                            <select>
+                            <select className="form-control" value ={varient} onChange={(e)=>{setvarient(e.target.value)}}>
                                 {pizza.varients.map(varient=>{
                                     return<option value={varient}>{varient}</option>
                                 })}
                             </select>
                         </div>
 
-                        <div className="w-100">
+                        <div className="w-100 m-1">
                             <p>Quantity</p>
-                                <select >
+                                <select className="form-control" value ={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
                                   {[...Array(10).keys()].map((x ,i)=>{
 
                                         return <option value={i+1}>{i+1}</option>
                                   })}  
                                 </select>
-                        </div>
+                         </div>
+                    </div>              
+                        
+                                  <div className="flix-container">
+                                        <div className="m-1 w-100">
+                                            <h5>Price : {pizza.prices[0][varient] * quantity}</h5>
+                                        </div>
+                                        <div>
+                                            <button className="btn">ADD TO CART</button>
+                                        </div>
+                                  </div>
+                        
 
-
-                    </div>
+                    
 
         </div>
     )
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -7,7 +7,7 @@ export default function Homescreen(){
         <div>
             <div className="row">
                 {pizzas.map(pizza=>{
-                    return <div className="col-md-4">
+                    return <div className="col-md-4 p-3">
                         <div>
                             <Pizza pizza={pizza}/>
                         </div>
