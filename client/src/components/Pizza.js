c1913967bce0709bd53cf95b4d0649078d630dac
Nipun Chandima <86118362+SANCS99@users.noreply.github.com>
2022-03-23 19:45:57 +0530

    home page (FE)
    FE all done

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,47 +1,83 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+export default function Pizza({ pizza }) {
+  const [quantity, setquantity] = useState(1);
+  const [varient, setvarient] = useState("small");
+  const [show, setShow] = useState(false);
 
-export default function Pizza({pizza}) {
-const[quantity, setquantity]=useState(1)
-const[varient, setvarient]=useState('small')
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    return(
-        <div style={{margin:"50px"}}className="shadow-lg p-3 mb-5 bg-white rounded">
-            <h4>{pizza.name}</h4>
-            <img src={pizza.image} className="img-fluid" style={{height:'200px' , width:'200px'}}/>
-
-                    <div className="flix-container">
-                        <div className="w-100 m-1">
-                            <p>Varients</p>
-                            <select className="form-control" value ={varient} onChange={(e)=>{setvarient(e.target.value)}}>
-                                {pizza.varients.map(varient=>{
-                                    return<option value={varient}>{varient}</option>
-                                })}
-                            </select>
-                        </div>
-
-                        <div className="w-100 m-1">
-                            <p>Quantity</p>
-                                <select className="form-control" value ={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
-                                  {[...Array(10).keys()].map((x ,i)=>{
-
-                                        return <option value={i+1}>{i+1}</option>
-                                  })}  
-                                </select>
-                         </div>
-                    </div>              
-                        
-                                  <div className="flix-container">
-                                        <div className="m-1 w-100">
-                                            <h5>Price : {pizza.prices[0][varient] * quantity}</h5>
-                                        </div>
-                                        <div>
-                                            <button className="btn">ADD TO CART</button>
-                                        </div>
-                                  </div>
-                        
-
-                    
-
+  return (
+    <div
+      style={{ margin: "50px" }}
+      className="shadow-lg p-3 mb-5 bg-white rounded"
+    >
+      <div onClick={handleShow}>
+        <h4>{pizza.name}</h4>
+        <img
+          src={pizza.image}
+          className="img-fluid"
+          style={{ height: "200px", width: "200px" }}
+        />
+      </div>
+      <div className="flix-container">
+        <div className="w-100 m-1">
+          <p>Varients</p>
+          <select
+            className="form-control"
+            value={varient}
+            onChange={(e) => {
+              setvarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((varient) => {
+              return <option value={varient}>{varient}</option>;
+            })}
+          </select>
         </div>
-    )
+
+        <div className="w-100 m-1">
+          <p>Quantity</p>
+          <select
+            className="form-control"
+            value={quantity}
+            onChange={(e) => {
+              setquantity(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => {
+              return <option value={i + 1}>{i + 1}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+
+      <div className="flix-container">
+        <div className="m-1 w-100">
+          <h5>Price : {pizza.prices[0][varient] * quantity}</h5>
+        </div>
+        <div>
+          <button className="btn">ADD TO CART</button>
+        </div>
+      </div>
+
+      <Modal show={show}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img src={pizza.image} className="img-fluid" style={{height:'300px'}}/>
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn" onClick={handleClose}>
+            close
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
