dbb0ad9858fd828245f4d6aa5189e05aa4473892
Nipun Chandima <86118362+SANCS99@users.noreply.github.com>
2022-03-29 20:07:27 +0530

    gg
    ok

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <Navbar/>
 
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Route path='/admin' component={Adminscreen}/>
-      </BrowserRouter>
+      </BrowserRouter> */}
       
       <Homescreen/>
     </div>
